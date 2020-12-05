3473f5e354edd3e1f057def53b49d550737e68c2
Tomás Marques <tomasm576@gmail.com>
2020-12-05 01:29:52 +0000

    Update

---
+++ main.py
@@ -0,0 +1,20 @@
+import camelot
+import numpy
+import pandas
+
+
+tables = camelot.read_pdf('notas.pdf',pages='1-end')
+print("Total tables extracted:", tables.n)
+
+for x in range(len(tables)):
+    print(tables[x].parsing_report)
+
+camelot.plot(tables[0], kind='text').show()
+camelot.plot(tables[0], kind='line').show()
+camelot.plot(tables[0], kind='contour').show()
+
+
+a= numpy.vstack((tables[1].df,tables[2].df,tables[3].df))
+
+print(a)
+pandas.DataFrame(a).to_csv("ile.csv")
